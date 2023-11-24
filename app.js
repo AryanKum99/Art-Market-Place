4ef350fb46e6955571bdc0893c8839d7fa01bca4
unknown <somdatta.vskp@gmail.com>
2023-11-24 13:16:09 +0530

    ejs files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const mongoSanitize = require('express-mongo-sanitize');
-
+const userRoutes = require('./routes/user');
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -20,5 +20,10 @@ app.use(mongoSanitize());
 //     res.locals.error = req.flash('error');
 //     next();
 // })
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.use('/user', userRoutes);
 
 module.exports = app;
