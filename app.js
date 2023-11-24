4e5beeb51bb4cf5dc326cb84df882ba33806418e
unknown <somdatta.vskp@gmail.com>
2023-11-24 23:44:08 +0530

    created ejs files

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/user');
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
